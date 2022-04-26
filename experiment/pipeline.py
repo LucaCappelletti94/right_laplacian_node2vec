13521fe6943242fb386dd5283d838a480b02cc6f
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2022-04-26 18:24:41 +0200

    Added normalization callback

--- experiment/pipeline.py
+++ experiment/pipeline.py
@@ -1,6 +1,14 @@
 from embiggen.pipelines import evaluate_embedding_for_edge_prediction
+from ensmallen import Graph
 from tqdm.auto import tqdm
 import pandas as pd
+
+def string_graph_normalization(graph: Graph) -> Graph:
+    """Apply standard STRING PPI normalization."""
+    return graph.filter_from_names(min_edge_weight=700)\
+        .drop_singleton_nodes()\
+        .sort_by_decreasing_outbound_node_degree()\
+        .divide_edge_weights(1000.0)
 
 def run_experiment():
     """Pipeline to execute the right laplacian experiments."""
@@ -37,7 +45,8 @@ def run_experiment():
                 return_weight=4.0,
                 explore_weight=0.25,
                 normalize_by_degree=normalize_by_degree
-            )
+            ),
+            graph_normalization_callback=string_graph_normalization
         )
         holdouts["normalization_name"] = normalization_name
         all_holdouts.append(holdouts)
